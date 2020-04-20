047872b89762a75daef355420dbbdac84a929801
Stakhiev-Alexander <sashastakhiev@gmail.com>
2020-04-21 00:37:19 +0300

    Add config file and refactor line indentation

---
+++ config.js
@@ -0,0 +1,7 @@
+let config = {
+  user: "c##alex",
+  password: "1917",
+  connectionString: "localhost:1521/xe"
+};
+
+module.exports = config;
--- login.js
+++ login.js
@@ -1,85 +1,83 @@
 const argon2 = require('argon2');
 var ipcRenderer = require('electron').ipcRenderer;
-// const electron = require('electron')
-// const {remote} = electron
-// const {Menu}  = remote.require('electron')
+let config = require('./config.js');
 
 document.getElementById("loginBtn").addEventListener("click", loginBtnHandler);
 
 
 document.onkeyup = function(e) {
-    if (e.which == 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("loginBtn").click();
-    } 
+  if (e.which == 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("loginBtn").click();
+  } 
 };
 
 
 async function loginBtnHandler() {
-    var oracledb = require('oracledb');
-    const uid = document.getElementById("uid")
-    const password = document.getElementById("password")
-
-    oracledb.getConnection ( 
-        {
-            user: "c##alex",
-            password: "1917",
-            connectionString: "localhost:1521/xe"
-        },
-        function(err, connection) {
-            if (err) {
-                console.error(err.message);
-                return;
-            }
-            connection.execute(
-                'SELECT * FROM USERS WHERE LOGIN = :login',
-                [uid.value],
-                function(err, result) {
-                    if (err) {
-                        console.error(err.message);
-                        doRelease(connection);
-                        return;
-                    }
-                    var hash = undefined;
-                    if (result.rows.length > 0) {
-                        hash = result.rows[0][1]
-                    }
-                    doRelease(connection); 
-                    try {
-                        const prom = argon2.verify(hash, password.value)
-                        prom.then((res) => {
-                            if (res) {
-                                console.log("yes")
-                                console.log(ipcRenderer)
-                                ipcRenderer.send('show-main')
-                            } else {
-                                console.log("no1")
-                            }
-                        },
-                        (err) => {
-                            console.log("no2")
-                        })
-                    } catch (err) {
-                        console.log("no3")
-                    }
-                }
-            );
+  var oracledb = require('oracledb');
+  const uid = document.getElementById("uid")
+  const password = document.getElementById("password")
+  
+  oracledb.getConnection ( 
+    {
+      user: config.user,
+      password: config.password,
+      connectionString: config.connectionString
+    },
+    function(err, connection) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      connection.execute(
+        'SELECT * FROM USERS WHERE LOGIN = :login',
+        [uid.value],
+        function(err, result) {
+          if (err) {
+            console.error(err.message);
+            doRelease(connection);
+            return;
+          }
+          var hash = undefined;
+          if (result.rows.length > 0) {
+            hash = result.rows[0][1]
+          }
+          doRelease(connection); 
+          try {
+            const prom = argon2.verify(hash, password.value)
+            prom.then((res) => {
+              if (res) {
+                console.log("yes")
+                console.log(ipcRenderer)
+                ipcRenderer.send('show-main')
+              } else {
+                console.log("no1")
+              }
+            },
+            (err) => {
+              console.log("no2")
+            })
+          } catch (err) {
+            console.log("no3")
+          }
         }
-    );
+      );
+    }
+  );
 }
-
-async function handleWrongInput () { 
     
-}
-
+    async function handleWrongInput () { 
+      
+    }
+    
 function doRelease(connection) {
-    connection.close(
-        function(err) {
-            if (err) {
-                console.log(err.message);
-            }
-        }
-    );
+  connection.close(
+    function(err) {
+      if (err) {
+        console.log(err.message);
+      }
+    }
+  );
 }
